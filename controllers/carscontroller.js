64cb7945c2a8fb4e83b69b4a20ee2a555b8802f5
pieter19998 <pieter1998@protonmail.com>
2021-06-23 20:14:03 +0200

    test coverage fix?

--- controllers/carscontroller.js
+++ controllers/carscontroller.js
@@ -1,7 +1,6 @@
 const Car = require('../models/car');
 const ErrorHandler = require('../utillib/errorhandler');
-const errorCatcherAsync = require('../utillib/asyncerrorcatcher');
-//get a specific car by id => /api/v1/car/:id
+
 exports.getCar = async (req, res, next) => {
     const car = await Car.findById(req.params.id);
 
@@ -19,9 +18,7 @@ exports.getCar = async (req, res, next) => {
     })
 };
 
-
-//get all active cars => /api/v1/callers
-exports.getCars = async (req, res, next) => {
+exports.getCars = async (req, res) => {
     const cars = await Car.find();
 
     res.status(200).json({
@@ -31,7 +28,6 @@ exports.getCars = async (req, res, next) => {
     })
 };
 
-//update a car => /api/v1/car/:id
 exports.updateCar = async (req, res, next) => {
     var car = null;
     try {
@@ -63,11 +59,9 @@ exports.updateCar = async (req, res, next) => {
     })
 };
 
-//delete a car => /api/v1/car/:id
-exports.deleteCar = async (req, res, next) => {
-    var car = null;
+exports.deleteCar = async (req, res) => {;
     try {
-        car = await Car.findById(req.params.id);
+        const car = await Car.findById(req.params.id);
     } catch (err) {
         res.status(500).json({
             success: false,
@@ -90,7 +84,6 @@ exports.deleteCar = async (req, res, next) => {
     })
 };
 
-//create a new car => /api/v1/car/register
 exports.registerCar = async (req, res, next) => {
     const body = req.body;
     if (body === undefined){
